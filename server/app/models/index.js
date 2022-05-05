16be5ea3c2ffbedb586da51f8351568e134ca9eb
aaakch0316 <aaakch0316@gmail.com>
2022-05-05 15:48:46 +0900

    server [+] connect mongoDB

---
+++ server/app/models/index.js
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+
+mongoose.Promise = global.Promise
+
+const db = {}
+db.mongoose = mongoose
+db.url = dotenv.MONGO_URI   
+// db.User = new UserModel(mongoose)
+
+export default db
--- server/server.js
+++ server/server.js
@@ -4,14 +4,32 @@ import dotenv from 'dotenv'
 
 import index from "./app/routes/index.js"
 
+import db from './app/models/index.js'
+
+import applyDotenv from './app/lambdas/applyDotenv.js'
+
+
 async function startServer(){
     const app = express()
-    
+    const {mongoUri, port, jwtSecret} = applyDotenv(dotenv)
     app.use(express.static('public'));
     app.use(express.urlencoded({extended: true}));
     app.use(express.json());
     app.use("/", index);
     app.use(morgan('dev'))
+    db
+    .mongoose
+    .connect(mongoUri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log('몽고DB 연결 성공')
+    })
+    .catch(err => {
+        console.log('몽고DB와 연결 실패', err)
+        process.exit();
+    });
 
     app.listen(5000, () => {
         console.log('***************** ***************** *****************')
