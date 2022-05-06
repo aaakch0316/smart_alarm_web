9c261a10a77a7f862ef332c6e684aa79a3ead671
aaakch0316 <aaakch0316@gmail.com>
2022-05-06 14:12:47 +0900

    server [+] alarmSchema

---
+++ server/app/models/Alarm.js
@@ -0,0 +1,14 @@
+export default function AlarmModel(mongoose) {
+    const alarmSchema = mongoose.Schema({
+        alarmer: {
+            type: Objectid,
+            required: true,
+            ref: 'User'
+        },
+        isCheck: {type: Boolean, required: true},
+        content: {type: String, required: true},
+        alertTime: {type: Date, required: true},
+        mp4Url: String
+    }, {timestamps: true})
+    return mongoose.model('User', userSchema)
+}
