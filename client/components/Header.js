81b1d6d45e59ecc64b345b11404f733aa972b4c2
aaakch0316 <aaakch0316@gmail.com>
2022-05-10 10:10:31 +0900

    Modal BaseLine and header data props

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,24 +1,31 @@
 // import { useDispatch } from "react-redux";
 import { PersonAddSVG } from "@/icons";
+import Button from '@mui/material/Button';
+import BasicModal from './BasicModal'
+
 // import { setModalOpen } from "@/modules";
 // import { setCallToken } from "@/modules";
 
-export function Header() {
+export function Header({data}) {
 	// const dispatch = useDispatch();
 
 	return (
 		<header className="header">
 			<h1 className="header__h1">
-				안녕하세요  <span>김명자님</span>
+				안녕하세요  <span>{data.userName}님</span>
 			</h1>
-			<button
-				className="btn btn__primary btn__icon"
+			<Button
+				variant="contained"
+				size="large"
+				className="btn__icon"
+				styles={{fontSize: 10}}
 				// onClick={() => {
 				// 	dispatch(setModalOpen(true));
 				// }}
 			>
-				<PersonAddSVG /> <span>알람추가</span>
-			</button>
+				<PersonAddSVG /> <b>알람 추가</b>
+			</Button>
+			<BasicModal />
             {/* <button onClick={alertHello}>hello</button> */}
 		</header>
 	);
