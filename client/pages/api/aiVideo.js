2132da869a52fbcf43a0269a889387419be742ff
aaakch0316 <aaakch0316@gmail.com>
2022-05-17 12:31:40 +0900

    client [+] update mp4 data

--- client/pages/api/aiVideo.js
+++ client/pages/api/aiVideo.js
@@ -43,62 +43,58 @@ export default async (req, res) => {
 				const videoKey = keyResponse.data.data.key
 				
 				
-				let videoUrl = 'https://user-images.githubusercontent.com/60080224/168462583-71a048fa-4447-422e-bf57-763b0766db0f.mp4'
+				let videoBaseUrl = 'https://user-images.githubusercontent.com/60080224/168462583-71a048fa-4447-422e-bf57-763b0766db0f.mp4'
 				
 				let flag ="waiting"
-				// while (true){
-				// 	console.log(1)
-				// 	if (flag ==="waiting" || flag !== 100){
-				// 		console.log(2)
-				// 		const videoResponse = await fetch("https://dev.aistudios.com/api/odin/findProject",{
-				// 			method: "POST",
-				// 			headers: {
-				// 				"Content-Type": "application/json",
-				// 			},
-				// 			body: JSON.stringify({
-				// 				"appId":"aistudios.com",
-				// 				"platform":"web",
-				// 				"isClientToken":true,
-				// 				"token":token,
-				// 				"uuid":"6443234b-77d5-4013-bfd6-bb9399f317d9",
-				// 				"sdk_v":"1.0",
-				// 				"clientHostname":"aistudios.com",
-				// 				"key": videoKey
-				// 			}),
-				// 		})
-				// 		console.log(3)
-				// 		const videoUrl = await videoResponse.json()
-				// 		flag = videoUrl.data.progress
-				// 		console.log(flag)
-				// 	}else{
-				// 		console.log('endendend')
-				// 		const videoResponse = await fetch("https://dev.aistudios.com/api/odin/findProject",{
-				// 			method: "POST",
-				// 			headers: {
-				// 				"Content-Type": "application/json",
-				// 			},
-				// 			body: JSON.stringify({
-				// 				"appId":"aistudios.com",
-				// 				"platform":"web",
-				// 				"isClientToken":true,
-				// 				"token":token,
-				// 				"uuid":"6443234b-77d5-4013-bfd6-bb9399f317d9",
-				// 				"sdk_v":"1.0",
-				// 				"clientHostname":"aistudios.com",
-				// 				"key": videoKey
-				// 			}),
-				// 		})
-				// 		console.log(3)
-				// 		const videoUrl = await videoResponse.json()
-				// 		console.log(videoUrl)
-				// 		console.log('endendend')
-				// 		break
-				// 	}
-				// }
+				while (true){
+					if (flag ==="waiting" || flag !== 100){
+						const videoResponse = await fetch("https://dev.aistudios.com/api/odin/findProject",{
+							method: "POST",
+							headers: {
+								"Content-Type": "application/json",
+							},
+							body: JSON.stringify({
+								"appId":"aistudios.com",
+								"platform":"web",
+								"isClientToken":true,
+								"token":token,
+								"uuid":"6443234b-77d5-4013-bfd6-bb9399f317d9",
+								"sdk_v":"1.0",
+								"clientHostname":"aistudios.com",
+								"key": videoKey
+							}),
+						})
+						const videoUrl = await videoResponse.json()
+						flag = videoUrl.data.progress
+						console.log(flag)
+					}else{
+						const videoResponse = await fetch("https://dev.aistudios.com/api/odin/findProject",{
+							method: "POST",
+							headers: {
+								"Content-Type": "application/json",
+							},
+							body: JSON.stringify({
+								"appId":"aistudios.com",
+								"platform":"web",
+								"isClientToken":true,
+								"token":token,
+								"uuid":"6443234b-77d5-4013-bfd6-bb9399f317d9",
+								"sdk_v":"1.0",
+								"clientHostname":"aistudios.com",
+								"key": videoKey
+							}),
+						})
+						console.log(3)
+						const videoUrl = await videoResponse.json()
+						console.log(videoUrl)
+						console.log('endendend')
+						return res.status(201).json({mp4Url : videoUrl.data.video});
+					}
+				}
 
-				console.log(videoUrl)
+				console.log(videoBaseUrl)
 
-				return res.status(201).json({mp4Url : videoUrl});
+				return res.status(201).json({mp4Url : videoBaseUrl});
 
                 // return res.status(200).json(keyResponse.data)
 			} catch (error) {
